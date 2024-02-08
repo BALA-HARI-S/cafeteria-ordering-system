33e8ab2a3450bf31265c2ca0b148d5bc2ec6986a
BALA-HARI-S <balahari754@gmail.com>
2024-02-08 16:59:29 +0530

    added name input validation and correct quantity calculation in update edit food items in cart order

--- cafeteria-ordering-system/src/main/java/net/breezeware/service/api/FoodItemManager.java
+++ cafeteria-ordering-system/src/main/java/net/breezeware/service/api/FoodItemManager.java
@@ -10,10 +10,9 @@ public interface FoodItemManager {
     /**
      * Create a New Food Item with some added information of the Food Item.
      * @param name - Food Name
-     * @param quantity - Food Quantity
      * @param price - Food price
      */
-    FoodItem createFoodItem(String name, int quantity, double price) throws CustomException;
+    FoodItem createFoodItem(String name, double price) throws CustomException;
     FoodItem retrieveFoodItem(String foodItemName) throws CustomException;
     List<FoodItem> retrieveAllFoodItems(boolean isOrderBy, int sortOrder, String columnName) throws CustomException;
     FoodItem updateFoodItemName(String newName, String foodItem) throws CustomException;
